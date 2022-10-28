13fe626d57029d12325750cd17daf011e82a9bde
Lee Congdon <lee@lcongdon.com>
2022-10-28 16:53:22 -0400

    Work tutorial

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,28 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{Delay, DelayMs, LedArray, OutputSwitch, entry};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let interval = 50_u16;
+    
+    // set initial status for LEDs in loop   
+    leds[0].on().ok();
+    leds[1].on().ok();
+
+    let mut index = 0;
+
+    loop {
+
+        leds[(index + 1) % 8].on().ok();
+        delay.delay_ms(interval);
+
+        leds[index].off().ok();
+        delay.delay_ms(interval);
+
+        index = (index + 1) % 8;
+    }
 }
